56e4b2bc72d4702f1d129704bad2fd47f4b3b4c3
GitHub Action <action@github.com>
2025-08-22 15:29:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/paulcombal/vuetranslationbatchupdaterintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/paulcombal/vuetranslationbatchupdaterintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.paulcombal.vuetranslationbatchupdaterintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.paulcombal.vuetranslationbatchupdaterintellijplugin.MyBundle
+import com.github.paulcombal.vuetranslationbatchupdaterintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
